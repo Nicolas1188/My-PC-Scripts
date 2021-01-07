8bf846b7d36059324bc688c10970cacdef8c8bd9
nico-castell <nicolas.a.castellan@outlook.com>
2021-01-06 21:53:35 -0300

    Update licence year.

--- MS Office/install.ps1
+++ MS Office/install.ps1
@@ -13,7 +13,7 @@
 
 # MIT License
 
-# Copyright (c) 2020 nico-castell
+# Copyright (c) 2021 nico-castell
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
